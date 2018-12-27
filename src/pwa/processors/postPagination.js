22cb4509e168f808a48cd93ab65460a443d42400
Eduardo Campaña <asrielo@gmail.com>
2018-12-27 18:52:39 +0100

    feat(pagination): autoscrolls on direct access to a page of the post
    fixes https://github.com/frontity/saturn-theme/issues/197

--- src/pwa/processors/postPagination.js
+++ src/pwa/processors/postPagination.js
@@ -2,10 +2,20 @@ import Waypoint from 'react-waypoint';
 
 let counter = 1;
 let lastPageEntered = 1;
+let lastHtmlTree = {};
 
 export default {
   test: ({ component, ignore }) => !ignore && component === 'h3',
-  process: (element, { stores: { connection, analytics } }) => {
+  process: (
+    element,
+    { stores: { connection, analytics }, item: { type, id }, htmlTree },
+  ) => {
+    // Resets the counter for each cycle of H2R.
+    if (lastHtmlTree !== htmlTree) {
+      counter = 1;
+      lastHtmlTree = htmlTree;
+    }
+
     const page = counter;
     counter += 1;
 
@@ -14,25 +24,40 @@ export default {
       component: Waypoint,
       props: {
         bottomOffset: '40%',
-        topOffset: '20%',
         scrollableAncestor: 'window',
-        fireOnRapidScroll: true,
         onEnter: () => {
           if (lastPageEntered === page) return;
 
           lastPageEntered = page;
 
-          const title = `${connection.head.title}${
-            page > 1 ? ` - Página ${page}` : ''
-          }`;
-          const location = `${connection.selectedItem.entity.link}${
-            page > 1 ? `/${page}` : ''
-          }`;
+          const initialTitle = connection.head.title;
+          const initialTitleHasPage = initialTitle.includes('- Página');
+          const titlePage = page > 1 ? ` - Página ${page}` : '';
+          const pageview = {
+            location: `${connection.selectedItem.entity.link}${
+              page > 1 ? `/${page}` : ''
+            }`,
+          };
 
-          analytics.sendPageView({ title, location });
+          if (initialTitleHasPage) {
+            pageview.title = initialTitle.replace(/ - Página \d+$/, titlePage);
+          } else {
+            pageview.title = `${connection.head.title}${titlePage}`;
+          }
+
+          analytics.sendPageView(pageview);
         },
       },
-      children: [{ ...element, ignore: true }],
+      children: [
+        {
+          ...element,
+          props: {
+            ...element.props,
+            'data-pagination-id': `${type}_${id}_${page}`,
+          },
+          ignore: true,
+        },
+      ],
     };
   },
 };
--- src/pwa/stores/base.js
+++ src/pwa/stores/base.js
@@ -1,10 +1,31 @@
 import { types, getParent, getEnv } from 'mobx-state-tree';
 
-export default types.model('Organization').actions(self => ({
-  addProcessors() {
-    const { h2r } = getParent(self, 1).theme;
-    const { processors } = getEnv(self).organization;
+export default types.model('Organization').actions(self => {
+  const root = getParent(self, 1);
 
-    processors.forEach(proc => h2r.addProcessor(proc, 'low'));
-  },
-}));
+  return {
+    addProcessors() {
+      const { h2r } = root.theme;
+      const { processors } = getEnv(self).organization;
+
+      processors.forEach(proc => h2r.addProcessor(proc, 'low'));
+    },
+    scrollToHeader() {
+      const { initialUrl } = root.build;
+      const [, page] = initialUrl.match(/\/(\d+)$/) || [null, null];
+
+      if (page) {
+        const { type, id } = root.connection.selectedItem;
+        const element = window.document.querySelector(
+          `h3[data-pagination-id='${type}_${id}_${page}']`,
+        );
+
+        if (element) {
+          // Calculates the scroll amount needed: distance from top minus navbar height.
+          const scrollAmount = element.getBoundingClientRect().top - 84;
+          window.scrollTo({ top: scrollAmount });
+        }
+      }
+    },
+  };
+});
