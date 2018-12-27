65746f93ca53219168912435252f52aee34bbd75
Eduardo Campaña <asrielo@gmail.com>
2018-12-27 18:04:51 +0100

    fix(export): fixes export to avoid react-universal-component error

--- src/pwa/client.js
+++ src/pwa/client.js
@@ -2,4 +2,7 @@ import Store from './stores/client';
 import env from './env';
 import { version } from '../../package.json';
 
+const ExtensionOrg42 = () => null;
+
+export default ExtensionOrg42;
 export { Store, env, version };
--- src/pwa/server.js
+++ src/pwa/server.js
@@ -2,4 +2,7 @@ import Store from './stores/server';
 import env from './env';
 import { version } from '../../package.json';
 
+const ExtensionOrg42 = () => null;
+
+export default ExtensionOrg42;
 export { Store, env, version };
